20520e282b476424f95d054a55d2846979cab850
liangkang <liangkang1436@163.com>
2023-02-19 08:26:39 +0800

    打包脚本改进

--- TDengineDataInsert/package-dockerTar.bat
+++ TDengineDataInsert/package-dockerTar.bat
@@ -1,4 +1,4 @@
-cd E:\DMProject\TDengineDataInsert
+cd E:\IDEAProject\TDengineLearn\TDengineDataInsert
 call mvn clean package -P nexusWithoutSnapshot jib:buildTar
 move target\tdengine-data-insert-image.tar C:\Users\wwwli\Desktop\tdengine-data-insert-image.tar
 pause
